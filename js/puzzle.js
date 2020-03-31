b7422830ab6da96c39477315f0fc81eb6f512a97
Daniil Asipenka <ocipenkoden@gmail.com>
2020-04-01 00:27:25 +0300

    feat: draw puzzle

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,13 +1,25 @@
 class Puzzle {
-	static puzzleSize = 4;
+	static puzzleSize = 5;
 
 	static createPuzzle() {
+		let array = new Array(this.puzzleSize**2).fill(1).map((a,i) => i).sort((a, b) => 0.5 - Math.random())
 		let puzzle = []
 		for(let i = 0; i < this.puzzleSize; i++){
-			puzzle.push(new Array(this.puzzleSize).fill(i*this.puzzleSize).map((a,j) => a+j))
-			puzzle[i].sort((a, b) => 0.5 - Math.random())
-			puzzle.sort((a, b) => 0.5 - Math.random())
+			puzzle.push(new Array(this.puzzleSize))
+			for(let j = 0; j < this.puzzleSize; j++){
+				puzzle[i][j] = array[j+this.puzzleSize*i]
+			}
 		}
+		this.drawPuzzle(puzzle);
+	}
+
+	static drawPuzzle(puzzle) {
+		puzzle.forEach((row,i) => {
+			document.querySelector('.puzzle').innerHTML += `<div class="puzzle__row"></div>`
+			row.forEach( gem => {
+				document.querySelector(`.puzzle`).lastChild.innerHTML += `<span>${gem}</span>`
+			})
+		})
 	}
 }
 
